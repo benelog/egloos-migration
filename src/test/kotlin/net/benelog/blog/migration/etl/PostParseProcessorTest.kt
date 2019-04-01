202c8aa6ea146848b05008fb653efb6e819e52ec
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2019-04-02 04:28:20 +0900

    assert구문 리팩토링

--- src/test/kotlin/net/benelog/blog/migration/etl/PostParseProcessorTest.kt
+++ src/test/kotlin/net/benelog/blog/migration/etl/PostParseProcessorTest.kt
@@ -9,8 +9,10 @@ import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder
 import org.springframework.core.io.ClassPathResource
 import org.springframework.core.io.Resource
 import org.springframework.oxm.jaxb.Jaxb2Marshaller
+import java.time.format.DateTimeFormatter
 
 class PostParseProcessorTest {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
 
     @Test
     fun `파싱이 된다`() {
@@ -24,16 +26,8 @@ class PostParseProcessorTest {
             assertThat(title).isEqualTo("Java에서 여러줄에 걸친 문자열 선언을 편하게 하는 @Multiline")
             assertThat(nick).isEqualTo("정상혁")
             assertThat(tags).isEqualTo("java,multiline-string,multiline")
-
-            with(createdAt){
-                assertThat(year).isEqualTo(2013)
-                assertThat(month.value).isEqualTo(1)
-                assertThat(dayOfMonth).isEqualTo(29)
-                assertThat(hour).isEqualTo(7)
-                assertThat(minute).isEqualTo(29)
-                assertThat(second).isEqualTo(4)
-            }
-        }
+            assertThat(createdAt.format(formatter)).isEqualTo("2013-01-29 07:29:04")
+           }
     }
 
     private fun buildItemReader(resource: Resource): StaxEventItemReader<Post> {
